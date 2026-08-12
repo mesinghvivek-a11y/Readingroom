93f00f4b390b0dbb078f153b16e418f627de0df0
mesinghvivek-a11y <mesinghvivek@gmail.com>
2026-08-12 15:04:48 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import OwnerPortal from "./pages/OwnerPortal";
+import StudentPortal from "./pages/StudentPortal";
+import AdminPortal from "./pages/AdminPortal";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/owner/*" element={<OwnerPortal />} />
+        <Route path="/student/*" element={<StudentPortal />} />
+        <Route path="/admin/*" element={<AdminPortal />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
